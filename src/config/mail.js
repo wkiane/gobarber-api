840a60c39e2fe0fd4c175de56d09d2ce3cecc0d9
willa kiane <willa_abreu@outlook.com>
2020-01-11 20:45:04 -0300

    configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '5bd4121e6bb1db',
+    pass: '1f08fc6d6091f2',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
