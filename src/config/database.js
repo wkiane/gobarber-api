2347ea6ee6ae0a69058381b60144b270178b53c6
willa <wkiane@pop-os.localdomain>
2020-01-18 00:14:02 -0300

    added port on database config

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
+  port: process.env.DB_PORT,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
